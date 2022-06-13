13a8b96db21040ebf71b9a802968bb5ca6894d4d
Atiways <Atiwaysbusiness@gmail.com>
2022-06-13 20:45:48 +0100

    finished second screen

--- lib/Screens/All.dart
+++ lib/Screens/All.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:tasks_flutter_app/constants/colors.dart';
 import 'package:tasks_flutter_app/models/Task.dart';
 
-class All extends StatelessWidget {
+class All extends StatefulWidget {
   final Task task;
 
   All(this.task);
 
+  @override
+  State<All> createState() => _AllState();
+}
+
+class _AllState extends State<All> {
+  bool value1 = false;
+  bool valueOne = false;
+  bool valueTwo = false;
+  bool valueThree = true;
+  bool valueFour = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,11 +34,165 @@ class All extends StatelessWidget {
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: [],
+                children: [
+                  Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 15.0, vertical: 30),
+                          child: Text(
+                            "Late",
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              CheckboxListTile(
+                                  title: Text('Call Max',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold)),
+                                  subtitle: Text(
+                                    "20:15-April 29",
+                                    style: TextStyle(color: Mycolor.red),
+                                  ),
+                                  value: this.value1,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      this.value1 = value!;
+                                    });
+                                  })
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height / 80,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 15.0, vertical: 30),
+                          child: Text(
+                            "Today",
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              CheckboxListTile(
+                                  title: Text('Practice Piano',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold)),
+                                  subtitle: Text(
+                                    "16:00",
+                                  ),
+                                  value:valueOne,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      valueOne = value!;
+                                    });
+                                  })
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              CheckboxListTile(
+                                  title: Text('Learn Spanish',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold)),
+                                  subtitle: Text(
+                                    "17:00",
+                                  ),
+                                  value: valueTwo,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      valueTwo = value!;
+                                    });
+                                  })
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height / 80,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 15.0, vertical: 30),
+                          child: Text(
+                            "Done",
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              CheckboxListTile(
+                                  title: Text('Finalize Presentation',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          decoration:
+                                              TextDecoration.lineThrough)),
+                                  subtitle: Text(
+                                    "9:00-11:30",
+                                  ),
+                                  value: valueThree,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      valueThree = value!;
+                                    });
+                                  }),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              CheckboxListTile(
+                                  title: Text('Football Practice',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          decoration:
+                                              TextDecoration.lineThrough)),
+                                  subtitle: Text(
+                                    "11:30-12:30",
+                                  ),
+                                  value: valueFour,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      valueFour = value!;
+                                    });
+                                  }),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           )
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 0,
+        onPressed: () {},
+        child: Icon(Icons.add),
       ),
     );
   }
@@ -61,14 +225,14 @@ class All extends StatelessWidget {
                 CircleAvatar(
                   backgroundColor: Mycolor.white,
                   child: Icon(
-                    task.iconData,
+                    widget.task.iconData,
                     color: Mycolor.blue,
                     size: 35,
                   ),
                 ),
                 SizedBox(height: MediaQuery.of(context).size.height / 60),
                 Text(
-                  task.title!,
+                  widget.task.title!,
                   style: TextStyle(
                       fontSize: 25,
                       fontWeight: FontWeight.bold,
@@ -76,7 +240,7 @@ class All extends StatelessWidget {
                 ),
                 SizedBox(height: MediaQuery.of(context).size.height / 60),
                 Text(
-                  task.subtitle!,
+                  widget.task.subtitle!,
                   style: TextStyle(color: Mycolor.white),
                 )
               ],
